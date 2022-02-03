d286773d5cea6bbcdc118ec5c4f79f248e4484d3
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2022-02-03 16:07:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 wget https://filestream.artemisfowl004.repl.co/6638009704132/Nature_Walls.pdf
 ebook-convert Nature_Walls.pdf test.epub
+wget https://filestream.artemisfowl004.repl.co/6775448657604/adobe%20scan%2027-jan-2022.pdf
+ebook-convert "adobe scan 27-jan-2022.pdf" test1.epub
 python3 main.py
